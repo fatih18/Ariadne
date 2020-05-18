112c7f26f5c851e327747acc303dddf300c86764
GokberkG <gokberk.gunduz@ug.bilkent.edu.tr>
2020-05-18 23:06:45 +0300

    feat: add ObstacleController / Implemented the template method

---
+++ Assets/Scripts/Gokberk/ObstackleController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstackleController : MonoBehaviour
+{
+	public PlayerController m_player;
+	public float m_damage;
+	
+
+
+	public void OnTriggerEnter(Collider other)
+	{
+        if (other.gameObject.GetComponent<PlayerController>() == m_player)
+        {
+			float phealth = m_player.getHealth();
+			phealth -= m_damage;
+
+			m_player.setHealth(phealth);
+		}		
+	}
+
+}
