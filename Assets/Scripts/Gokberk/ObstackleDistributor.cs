04bb57432eabdf02e58026394ac433f4903081e3
GokberkG <gokberk.gunduz@ug.bilkent.edu.tr>
2020-05-18 23:10:44 +0300

    feat: add abstract ObstacleDistributor / Implemented the template method

---
+++ Assets/Scripts/Gokberk/ObstackleDistributor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class ObstackleDistributor : MonoBehaviour
+{
+
+    public void distribute()
+    {
+        //TODO implement a obstacle distribution algorithm.
+    }
+    abstract public void setMesh(Mesh mesh);
+    abstract public void setMaterial(Material material);
+    //abstract public void setBehavior(Behavior behavior);
+
+}
