02a8f23ec71e859d727fc8d91bedf74d0e46a082
GokberkG <gokberk.gunduz@ug.bilkent.edu.tr>
2020-05-18 23:11:55 +0300

    feat: add SpikeDistributor extends ObstacleDistributor / Implemented the template method

---
+++ Assets/Scripts/Gokberk/SpikeDistributor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeDistributor : ObstackleDistributor
+{
+    public Mesh mesh;
+    public Material material;
+    //public Behavior behavior;
+    
+    public override void setMesh(Mesh mesh)
+    {
+        mesh = this.mesh;
+    }
+    public override void setMaterial(Material material)
+    {
+        material = this.material;
+    }
+    //public void setBehavior(Behavior behavior)
+   //// }
+    
+}   
+
